611d0c8d5dbdbd85fa281c8e42ee2e97137aaa06
Aakash Venkatasubramanian <aakashvenkatasubramanian@gmail.com>
2020-04-15 22:52:19 +0530

    logout from all devices

--- src/routers/user.js
+++ src/routers/user.js
@@ -26,6 +26,29 @@ router.post('/user/login', async (req, res) => {
     }
 })
 
+router.post('/user/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        
+        res.send({result:'Logout successful', status:'success'})
+    } catch (error) {
+        res.status(500).send({result:error, status:'failed'})
+    }
+})
+
+router.post('/user/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send({result:'Logout successful', status:'success'})
+    } catch (error) {
+        res.status(500).send({result:error, status:'failed'})
+    }
+})
+
 router.get('/user/profile', auth, async (req, res) => {
     res.send({result:req.user, status:'success'})
 })
